79d991b090820fe6b48a99000bc1f301d5525de7
Alexander <a.fomin1@g.nsu.ru>
2025-03-03 11:59:12 +0700

    SYSTEMS 11.1 Фикс:     - задал приоритет для mongo-primary, чтобы он был всегда primary

--- scripts/init-replica.js
+++ scripts/init-replica.js
@@ -1,7 +1,7 @@
 rs.initiate({
     _id: "rs0",
     members: [
-        { _id: 0, host: "mongo-primary:27017" },
+        { _id: 0, host: "mongo-primary:27017", priority: 3 },
         { _id: 1, host: "mongo-secondary-1:27017" },
         { _id: 2, host: "mongo-secondary-2:27017" }
     ]
